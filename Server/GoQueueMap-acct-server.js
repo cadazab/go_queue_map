37bd1fc88de9d7b4cd69174411e2028a3113f823
Camilo <cadazab@gmail.com>
2017-11-24 16:53:18 +0100

    index.html

--- Server/GoQueueMap-acct-server.js
+++ Server/GoQueueMap-acct-server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 
 var authenticationAcct = authentication.Acct
 
-app.use(function(req, res, next) {
+app.use(function(req, res, next) {c
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
